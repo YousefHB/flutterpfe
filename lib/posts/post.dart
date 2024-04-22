875c4c9baf8c6d27a2f378a3fc613c6adaea8d6d
Hbaiebyoussef <youssef18.hbaieb3@gmail.com>
2024-04-22 17:34:57 +0100

    ta7sin l scroll

--- lib/posts/post.dart
+++ lib/posts/post.dart
@@ -22,6 +22,10 @@ class Post extends StatefulWidget {
   _PostState createState() => _PostState();
 }
 
+const Color myCustomColor = Color(0xFF009EE2);
+const Color myCustomColor1 = Color(0xFF38B8c4);
+const String myfont = 'ArialRounded';
+
 class _PostState extends State<Post> {
   bool showFullText = false;
   bool showReactionRow = false;
@@ -68,253 +72,266 @@ class _PostState extends State<Post> {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-        side: BorderSide(color: Colors.blue, width: 2.0),
-      ),
-      child: Container(
-        width: screenWidth * 0.9,
-        height: showCommentSection
-            ? 700
-            : 400, // Augmentez la hauteur si la section de commentaire est visible
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 10.0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color:
-                              Colors.grey, // Change the border color as needed
-                          width: 2,
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          side: BorderSide(color: myCustomColor, width: 1.0),
+        ),
+        child: Container(
+          width: screenWidth * 0.8,
+          height: showCommentSection
+              ? 700
+              : 420, // Augmentez la hauteur si la section de commentaire est visible
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color:
+                                myCustomColor1, // Change the border color as needed
+                            width: 1,
+                          ),
                         ),
-                      ),
-                      child: widget.profilePhotoUrl != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                  30), // Half of the width/height to make it circular
-                              child: Image.network(
-                                widget.profilePhotoUrl!,
-                                width: 60,
-                                height: 60,
+                        child: widget.profilePhotoUrl != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(
+                                    30), // Half of the width/height to make it circular
+                                child: Image.network(
+                                  widget.profilePhotoUrl!,
+                                  width: 50,
+                                  height: 50,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                            : Image.asset(
+                                'assets/image/profile.png',
+                                width: 50,
+                                height: 50,
                                 fit: BoxFit.cover,
                               ),
-                            )
-                          : Image.asset(
-                              'assets/image/profile.png',
-                              width: 60,
-                              height: 60,
-                              fit: BoxFit.cover,
-                            ),
+                      ),
                     ),
                   ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.firstName! + " " + widget.lastName!,
+                        style: TextStyle(
+                            fontFamily: myfont,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        calculateTimestamp(),
+                        style: TextStyle(
+                            color: myCustomColor1,
+                            fontFamily: myfont,
+                            fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  /*Image.asset(
+                    "assets/image/menudot.png",
+                    width: 15,
+                    height: 15,
+                  )*/
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 20, 25, 10),
+                child: Text(
+                  textAlign: TextAlign.start,
+                  showFullText
+                      ? widget.content
+                      : widget.content.split(' ').take(20).join(' ') + '...',
+                  style: TextStyle(
+                    fontFamily: myfont,
+                    fontSize: 12,
+                  ),
                 ),
-                Column(
+              ),
+              if (widget.content.split(' ').length > 20)
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      showFullText = !showFullText;
+                    });
+                  },
+                  child: Text(
+                    showFullText ? 'Voir moins' : 'Voir plus',
+                    style: TextStyle(color: myCustomColor),
+                  ),
+                ),
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: widget.images.map((imageUrl) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25.0, vertical: 0.0),
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+              if (showReactionRow)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(widget.firstName! + " " + widget.lastName!),
-                    Text(
-                      calculateTimestamp(),
-                      style: TextStyle(color: Colors.blue),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          showReactionRow = false;
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/image/Rjaime.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          showReactionRow = false;
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/image/jadore.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          showReactionRow = false;
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/image/bravo.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          showReactionRow = false;
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/image/trist.png',
+                        width: 50,
+                        height: 50,
+                      ),
                     ),
                   ],
                 ),
-                Image.asset(
-                  "assets/image/menudot.png",
-                  width: 15,
-                  height: 15,
-                )
-              ],
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 0.0),
-              child: Text(
-                showFullText
-                    ? widget.content
-                    : widget.content.split(' ').take(20).join(' ') + '...',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
-                ),
-              ),
-            ),
-            if (widget.content.split(' ').length > 20)
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    showFullText = !showFullText;
-                  });
-                },
-                child: Text(
-                  showFullText ? 'Voir moins' : 'Voir plus',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-            Expanded(
-              child: SizedBox(
-                width: double.infinity,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: widget.images.map((imageUrl) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 25.0, vertical: 0.0),
-                      child: Image.network(
-                        imageUrl,
-                        width: 300,
-                        height: 300,
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-            if (showReactionRow)
-            Row(
-              children: [
-                GestureDetector(
-                  onTap : () {
+              Row(
+                children: [
+                  GestureDetector(
+                    onLongPress: () {
                       setState(() {
-                      showReactionRow = false; 
-                    });
-                  },
-               child: Image.asset(
-                    'assets/image/Rjaime.png',
-                    width: 50,
-                    height: 50,
-                  ),
+                        showReactionRow =
+                            true; // Afficher la rangée de réaction
+                      });
+                    },
+                    child: Image.asset(
+                      'assets/image/jaime.png',
+                      width: 80,
+                      height: 90,
+                    ),
                   ),
                   GestureDetector(
-                  onTap : () {
+                    onTap: () {
                       setState(() {
-                      showReactionRow = false; 
-                    });
-                  },
-               child: Image.asset(
-                    'assets/image/jadore.png',
-                    width: 50,
-                    height: 50,
-                  ),
-                  ),
-                 GestureDetector(
-                  onTap : () {
-                      setState(() {
-                      showReactionRow = false; 
-                    });
-                  },
-               child: Image.asset(
-                    'assets/image/bravo.png',
-                    width: 50,
-                    height: 50,
-                  ),
+                        showCommentSection =
+                            !showCommentSection; // Afficher ou masquer la section de commentaire
+                      });
+                    },
+                    child: Image.asset(
+                      'assets/image/commentaire.png',
+                      width: 140,
+                      height: 140,
+                    ),
                   ),
                   GestureDetector(
-                  onTap : () {
-                      setState(() {
-                      showReactionRow = false; 
-                    });
-                  },
-               child: Image.asset(
-                    'assets/image/trist.png',
-                    width: 50,
-                    height: 50,
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/image/partager.png',
+                      width: 90,
+                      height: 90,
+                    ),
                   ),
-                  ),
-              ],
-            ),
-            Row(
-              children: [
-                GestureDetector(
-                  onLongPress: () {
-                    setState(() {
-                      showReactionRow = true; // Afficher la rangée de réaction
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/image/jaime.png',
-                    width: 80,
-                    height: 90,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      showCommentSection =
-                          !showCommentSection; // Afficher ou masquer la section de commentaire
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/image/commentaire.png',
-                    width: 140,
-                    height: 140,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/image/partager.png',
-                    width: 90,
-                    height: 90,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Image.asset(
-                    'assets/image/enregistrement.png',
-                    width: 60,
-                    height: 30,
-                  ),
-                ),
-              ],
-            ),
-            if (showCommentSection) // Afficher la section de commentaire si nécessaire
-              Container(
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: commentController,
-                      decoration: InputDecoration(
-                        hintText: 'Entrez votre commentaire',
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              comments.add(commentController.text);
-                              commentController.clear();
-                            });
-                          },
-                          icon: Icon(Icons.send),
+                  /* GestureDetector(
+                    onTap: () {},
+                    child: Image.asset(
+                      'assets/image/enregistrement.png',
+                      width: 60,
+                      height: 30,
+                    ),
+                  ),*/
+                ],
+              ),
+              if (showCommentSection) // Afficher la section de commentaire si nécessaire
+                Container(
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: commentController,
+                        decoration: InputDecoration(
+                          hintText: 'Entrez votre commentaire',
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                comments.add(commentController.text);
+                                commentController.clear();
+                              });
+                            },
+                            icon: Icon(Icons.send),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: comments.map((comment) {
-                        return Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 60.0, vertical: 0.0),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(comment),
-                        );
-                      }).toList(),
-                    ),
-                  ],
+                      SizedBox(height: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: comments.map((comment) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 60.0, vertical: 0.0),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(comment),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
